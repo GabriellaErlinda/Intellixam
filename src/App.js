d6a04b17d78bafedf6f06f8a49f65b2aced930b4
Gabriellae11 <gabriella8803@gmail.com>
2025-05-05 00:12:05 +0700

    feat: added basename route

--- src/App.js
+++ src/App.js
@@ -155,7 +155,7 @@ function App() {
   return (
     // 3. Provide the authentication state and functions to the rest of the app
     <AuthContext.Provider value={{ ...authState, login: handleLogin, logout: handleLogout }}>
-      <Router>
+      <Router basename="/Intellixam">
         <Routes>
           {/* --- Public Routes (Accessible without login) --- */}
           <Route
