547f22782c4c901b731de2b97cc6cecb616e4431
Gabriellae11 <gabriella8803@gmail.com>
2025-05-06 18:33:22 +0700

    fix: user avatar

--- src/layouts/StudentLayout.js
+++ src/layouts/StudentLayout.js
@@ -145,8 +145,8 @@ function StudentLayout({ onLogout }) {
                    <Box sx={{ flexGrow: 0 }}>
                        <Tooltip title="User Options">
                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                               {/* Placeholder Avatar */}
-                                <Avatar sx={{ width: { xs: 80, sm: 100, md: 120 }, height: { xs: 80, sm: 100, md: 120 }, background: `linear-gradient(45deg, #7b4dff 30%, #3da9fc 90%)`, color: '#fff', fontSize: '3rem' }}>
+                               {/* Placeholder Avatar */}                               
+                                <Avatar sx={{background: `linear-gradient(45deg, #7b4dff 30%, #3da9fc 90%)`, color: '#fff', fontSize: '3rem' }}>
                                     <PersonIcon fontSize="inherit"/>
                                 </Avatar>
                            </IconButton>
