b684b5071579e370bb931fd79fa964e65eb826fc
Nicholas Marco Weinandra <nicoweinandra@gmail.com>
2025-06-09 14:41:23 +0700

    fix: update api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL for your backend API
-const API_URL = 'http://localhost:5001/api'; // Your Flask backend URL
+const API_URL = 'http://10.4.90.33:5001/api'; // Your Flask backend URL
 
 const apiClient = axios.create({
   baseURL: API_URL,
